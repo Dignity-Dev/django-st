b0b6b9118edabcdcb87e308075075a77f98bed94
Basheer Haadi <bashirhaadiy@gmail.com>
2025-11-27 23:18:40 +0100

    templating

--- playground/views.py
+++ playground/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse as HTTPResponse
 
 def say_hello(request):
-    return HTTPResponse("hello world")
+    return render(request, 'hello.html', {'name': 'Haadi'})
